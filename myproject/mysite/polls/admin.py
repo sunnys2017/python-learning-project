a3141335a5c51ecd55117a69ddbb74b9a2093f72
Sunny <sunnysun2108@gmail.com>
2018-03-19 20:40:17 -0400

    part 6, customize admin page, add search, filter, order, style

--- myproject/mysite/polls/admin.py
+++ myproject/mysite/polls/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
+
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	#fields = ['pub_date', 'question_text']
+	search_fields = ['question_text']
+	list_filter = ['pub_date']
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	fieldsets = [
+		(None,	{'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
 
 #tell the admin that Question objects have an admin interface
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
